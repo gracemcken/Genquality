a5c4b22eb7f71f3dfc94c1d4e45aeab120d03db0
Raivis Petrovskis <rapet80@gmail.com>
2022-09-07 15:10:03 +0100

    Add Quiz base

---
+++ assets/quiz/script.js
@@ -0,0 +1,25 @@
+// **Quiz randomized questions with yes no answer choices**
+
+// TODO: 1 import necessary DOM elements
+
+// TODO: 2 import json or dictionary data-set
+
+// TODO: 3 Create function to generate random number between 1 and Length of a data-set
+
+// TODO: 4 Select question from data-set based on random number generated earlier
+
+// TODO: 5 Create function to render question card element on the screen
+
+// TODO: 6 Add event listeners to the answer 'Yes' and 'No' buttons
+
+// TODO: 7 memorize question index number to avoid duplicate questions in the future
+
+// TODO: 8 Keep right and wrong answer count to display percentage at the end of the quiz
+
+// **Question randomized background color render**
+
+// TODO: 9 create list of colors 
+
+// TODO: 10 create function to generate random color based on color list
+
+// TODO: 11 based on click event o a button add class to the 'main-container' to change color
