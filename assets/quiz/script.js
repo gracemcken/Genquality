f1ba439439612d294ed08fe6729c33fed43d4c76
Raivis Petrovskis <rapet80@gmail.com>
2022-09-09 17:55:46 +0100

    addeg ending to quiz

--- assets/quiz/script.js
+++ assets/quiz/script.js
@@ -5,7 +5,7 @@ questions = [{
     question: "Women Negotiate for Raises and Promotions as Often as Men Do?",
     explanation: "In the last few years while 29% of men had negotiated for a raise, 31% of women had done the same. And 36% of men negotiated for a promotion compared to 37% of women.",
     answer: true
-},{
+}, {
     question: "Gender inequality is not an issue in developed countries?",
     explanation: "Although many countries have made progress on some aspects of gender equality, inequality remains high. In the US, there are just 66 women for every 100 men in leadership and managerial positions, and women do almost double the unpaid care work that men do.",
     answer: false
@@ -48,12 +48,12 @@ let canClick = true;
 
 
 // Function to generate random number between 1 and Length of a data-set
-function getRandomQuestion(){
+function getRandomQuestion() {
     qLength = questions.length;
     indexOfQuestion = Math.floor(Math.random() * qLength);
-    
+
     // TODO: 4 Select question from data-set
-    if (questions.length > 0){   
+    if (questions.length > 0) {
         randomQuestion = questions[indexOfQuestion];
         questions.splice(indexOfQuestion, 1);
         answer = randomQuestion.answer
@@ -64,37 +64,48 @@ function getRandomQuestion(){
     }
 }
 
-function charCount(str){
+function charCount(str) {
     return str.length;
 }
 
+// Answer 8 quiz questions to test your knowledge, 
+// by the end of the quiz you will know how much you know about equality
 // Function to render question card element on the screen
-function renderCard(){
+function renderCard() {
     questionEl = document.querySelector('#quiz-El');
+    quizText = document.querySelector('.quiz-text');
     buttonContainer = document.querySelector('.button-container');
     container = document.querySelector('.answer-popup');
+
+    percentage = calcAnswerPercentage(rightAnswer, wrongAnswer)
+    percentageInt = Math.floor(percentage)
+
     container.innerHTML = '';
     questionEl.innerHTML = '';
 
     // Call Get random question function
     question = getRandomQuestion();
 
-    if (question != null){
+    if (question != null) {
+        // Render question
         questionEl.innerHTML = `
         <div class="question">
-        <h2> ${questions.length + 1} question${ questions.length + 1 == 1 ? '' : 's'} remaining</h2>
+        <h1> Q <span class="">${questions.length + 1}</span> of <span>8</span></h1>
         </h1><p> ${question.question}</p>
         </div> 
         `;
-    } else { 
+    } else {
         // If there are no more questions, render the result card
+
         buttonContainer.innerHTML = ''
         questionEl.innerHTML = `
         <div class="question">
-        <h1>End of quiz</h1>
-        <p>Right answers: ${rightAnswer}</p>
-        <p>Wrong answers: ${wrongAnswer}</p>
-        <p>Percentage: ${calcAnswerPercentage(rightAnswer, wrongAnswer).toFixed(0)}%</p>
+            <h1> <span>${percentageInt}%</span> of <span>100%</span></h1>
+            <p>You got ${rightAnswer} out of ${rightAnswer + wrongAnswer} questions right</p>
+            <div class="button-container">
+                <a href="quiz.html" type="button" class="border-right true" aria-label="Restart quiz"><i class="fa-solid fa-rotate-right"></i></a>
+                <a href="resources.html" type="button" class="false" aria-label="Leave"><i class="fa-solid fa-right-to-bracket"></i></a>
+            </div>
         </div> 
         `;
         container.remove();
@@ -102,7 +113,7 @@ function renderCard(){
 }
 
 // Calculate percentage of right answers
-function calcAnswerPercentage(rightAnswer, wrongAnswer){
+function calcAnswerPercentage(rightAnswer, wrongAnswer) {
     let totalAnswer = rightAnswer + wrongAnswer
     let percentage = (rightAnswer / totalAnswer) * 100
     return percentage
@@ -123,7 +134,7 @@ function showElem(elem) {
 
 
 // Render explanation in the card if answer was wrong
-function renderExplanation(){
+function renderExplanation() {
     const container = document.querySelector('.answer-popup')
     const btnNext = document.querySelector('.btn-next')
 
@@ -136,48 +147,48 @@ function renderExplanation(){
     // Show explanation
     showElem(container);
     showElem(btnNext);
-    
+
     // Set timer to hide explanation
-    setTimeout(function(){
+    setTimeout(function () {
         hideElem(btnNext);
         hideElem(container);
     }, timer);
 
     // Hidden but visible by screen readers
-    setTimeout(function(){
+    setTimeout(function () {
         container.classList.add('hidden')
     }, timer + 300);
 
     // Render explanation
     container.innerHTML = `
             <div class="quiz-card">
-                <h3 class="wrong">Wrong answer</h3>
+                <h4 class="wrong text-center"><i class="fa-solid fa-exclamation"></i></h4>
                 <p>${explanation}</p>
             </div>
            
             `;
 
     // clear element after specified time
-    setTimeout(function(){
+    setTimeout(function () {
         container.innerHTML = ''
     }, timer + 300);
-    
+
 }
 
 
 // Wrong answer effect function
 // Get card element and add wrong class to it
-function buzzEffect(answer){
+function buzzEffect(answer) {
     cardWrapper = document.querySelector('.card-wrapper')
     quizCard = document.querySelector('.quiz-card')
-    if (answer == false){
+    if (answer == false) {
         quizCard.classList.add('buzz-wrong')
         cardWrapper.classList.add('buzz-wrong')
     } else {
         quizCard.classList.add('buzz-right')
         cardWrapper.classList.add('buzz-right')
     }
-    setTimeout(function(){
+    setTimeout(function () {
         quizCard.classList.remove('buzz-wrong')
         cardWrapper.classList.remove('buzz-wrong')
         cardWrapper.classList.remove('buzz-right')
@@ -188,23 +199,34 @@ function buzzEffect(answer){
 
 
 // Add event listeners to the answer 'Yes' and 'No' buttons
-function addListeners(){
-    renderCard();
+init = 0;
+
+function addListeners() {
     const btnTrue = document.querySelector('#btn-true');
     const btnFalse = document.querySelector('#btn-false');
     const btnNext = document.querySelector('.btn-next');
 
     btnTrue.addEventListener('click', function () {
-        if (canClick){
-            checkAnswer(true);
-            canClick = false;
+        if (canClick) {
+            if (init == 0) {
+                renderCard();
+                init = 1;
+            } else {
+                checkAnswer(true);
+                canClick = false;
+            }
         }
     });
 
     btnFalse.addEventListener('click', function () {
-        if (canClick){
-            checkAnswer(false);
-            canClick = false;
+        if (canClick) {
+            if (init == 0) {
+                document.location.href = 'https://raivis80.github.io/Genquality/index.html';
+                init = 1;
+            } else {
+                checkAnswer(false);
+                canClick = false;
+            }
         }
     });
 
@@ -220,29 +242,28 @@ addListeners();
 
 // Create function to check if the answer is correct
 // Keep right and wrong answer count to display percentage at the end of the quiz
-function checkAnswer(inputAnswer){
-    if (inputAnswer == answer){
+function checkAnswer(inputAnswer) {
+    if (inputAnswer == answer) {
         buzzEffect(true);
         rightAnswer++;
         renderNextQuestion(100);
-    }else{
+    } else {
         buzzEffect(false);
         wrongAnswer++;
         renderExplanation();
         // Clear next question timer
-        renderNextQuestion(timer, true);
+        renderNextQuestion(timer, false);
     }
-    
 }
 
 // Render next question after specified time
-function renderNextQuestion(timer, state){
-    timeOut = setTimeout(function(){
+function renderNextQuestion(timer, state) {
+    timeOut = setTimeout(function () {
         renderCard();
         canClick = true;
     }, timer);
-
-    if (state == true){
+    // If state is false, clear timeout
+    if (state == true) {
         clearTimeout(timeOut);
     }
 }
@@ -251,14 +272,14 @@ function renderNextQuestion(timer, state){
 
 // Create list of colors
 // Function to generate random color based on color list
-function getRandomColor(){
+function getRandomColor() {
     const colors = ['#EDDCD2', '#FFF1E6', '#FDE2E4', '#FAD2E1', '#C5DEDD', '#DBE7E4', '#F0EFEB', '#D6E2E9', '#BCD4E6', '#99C1DE']
     let randomColor = colors[Math.floor(Math.random() * colors.length)];
     return randomColor;
 }
 
 // Based on click event o a button add class to the 'main-container' to change color
-function changeColor(){
+function changeColor() {
     let randomColor = getRandomColor();
     let mainContainer = document.querySelector('.main-container');
     cardWrapper = document.querySelector('.card-wrapper');
